95ca83e0db4e597f3fd052e2cf31b366c4b71880
hongphucbk <phuchong94@gmail.com>
2020-06-05 13:04:03 +0700

    add service 1

--- service.js
+++ service.js
@@ -8,7 +8,7 @@ var svc = new Service({
   name:'AucontechGateway',
   description: 'The gateway read data from Flexy by Aucontech',
   //script: 'E:\\Aucontech\\02. Project\\02. Linde Malaysia\\Project\\GatewayIOT_Demo\\index1.js',
-  script: require('path').join(__dirname,'index1.js')
+  script: require('path').join(__dirname,'index.js')
 });
  
 // Listen for the "install" event, which indicates the
