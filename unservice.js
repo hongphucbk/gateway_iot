c0ffe2cc337b94f2fff8fb7ee0d3d05bc103836b
hongphucbk <phuchong94@gmail.com>
2020-06-05 13:03:41 +0700

    add service

--- unservice.js
+++ unservice.js
@@ -2,7 +2,7 @@ var Service = require('node-windows').Service;
 // Create a new service object
 var svc = new Service({
   name:'AucontechGateway',
-  script: require('path').join(__dirname,'index1.js')
+  script: require('path').join(__dirname,'index.js')
 });
  
 // Listen for the "uninstall" event so we know when it's done.
